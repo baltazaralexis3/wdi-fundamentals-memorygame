0478eceb3d2558a779bcf4a0025ec7b63b1db2f7
Alexis Baltazar <alexisbaltazar@Alexiss-MacBook-Pro.local>
2019-01-18 00:19:01 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,21 @@
-console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "king";
-var cardFour = "queen";
-console.log("User flipped" + cardTwo);
-console.log("User flipped" + cardFour);
+
+var cards = ["queen","queen","king","king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+
+cardsInPlay.push("cardOne");{
+	console.log("user flipped queen");
+}
+
+var cardTwo = cards[2];
+cardsInPlay.push("cardTwo");{
+	console.log("user flipped king");
+}
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay [1]) {
+		alert("you found a match!");
+	} else {
+		alert("sorry, try again");
+	}
+}
