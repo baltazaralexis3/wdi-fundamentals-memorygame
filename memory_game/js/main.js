b818162b8ce057434d6a0616448efd4518c7b150
Alexis Baltazar <alexisbaltazar@alexiss-mbp.home>
2019-01-19 15:31:21 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,5 +1,27 @@
 
-var cards = ["queen","queen","king","king"];
+var cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png",
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png",
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png",
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png",
+}
+
+];
 var cardsInPlay = [];
 
 var checkForMatch = function() {
@@ -12,13 +34,21 @@ console.log("Sorry, try again.");
 
 var flipCard = function(cardId) {
 
-	console.log("user flipped " + cards[cardId]);
+	console.log("user flipped " + cards[cardId].rank);
 
-	cardsInPlay.push(cards[cardId])
+	cardsInPlay.push(cards[cardId].rank);
+
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
+
+	
 
 if (cardsInPlay.length === 2) {
 	checkForMatch();
 }
 }
+
+
+
 flipCard(0);
 flipCard(2);
